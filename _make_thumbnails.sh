157b33748cb731284391b9f195d5c23168ad6945
anubhav <av72@uw.edu>
2025-12-16 16:19:31 -0800

    testing

--- _make_thumbnails.sh
+++ _make_thumbnails.sh
@@ -1,6 +1,6 @@
 for file in images/*.{jpg,png,gif}; do
     [ -f "$file" ] && {
         filename=$(basename "$file")
-        [ ! -f "tn/images/$filename" ] && ffmpeg -i "$file" -vf scale=250:-1 "tn/images/$filename"
+        [ ! -f "tn/images/$filename" ] && ffmpeg -i "$file" -vf scale=400:-1 "tn/images/$filename"
     }
 done
